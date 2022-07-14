2e300e225ad5e11dd5732f7b8a07891f1b86697f
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-07-14 14:15:16 +0300

    practise writing comments and content

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -6,8 +6,10 @@ function BlogPost() {
   return (
     <div id="blog-post">
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <BlogContent articleText="Made my first comment on react"/>
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
+      <Comment commentText="A universal truth. - Angela Kanyi" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
     </div>
   );
